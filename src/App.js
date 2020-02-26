600392a4e2cc59e947ec58db5aad0bb315d0e8c7
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-27 00:52:26 +0100

    Update category page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import './App.css';
-import categories from "./data/categories.json";
+import categoriesData from "./data/categories.json";
 import Navigation from "./components/Navigation";
 import CategoriesPage from "./page/CategoriesPage";
 
 function App() {
     const [currentPage, setCurrentPage] = useState(0);
+    const [categories, setCategories] = useState(categoriesData);
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [selectedGoal, setSelectedGoal] = useState(null);
     const [mascotView, setMascotView] = useState(false);
@@ -13,7 +14,11 @@ function App() {
   return (
       <div>
           {currentPage === 1 &&
-              <CategoriesPage/>
+              <CategoriesPage
+                categories={categories}
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+              />
           }
           <Navigation
             setCurrentPage={setCurrentPage}
--- src/page/CategoriesPage.js
+++ src/page/CategoriesPage.js
@@ -1,20 +1,48 @@
 import React, {useState} from 'react';
 import categories from "./../data/categories.json";
 
-const CategoriesPage = () => {
+const CategoriesPage = ({ categories, selectedCategory, setSelectedCategory }) => {
     console.log("zerzer")
     return (
         <div className={"container"}>
-            <div>
-                Thèmes
-            </div>
-            <div className="categories-wrapper">
-                {categories.map((category, index) => (
-                    <div key={index}>
-                        {category.title}
+            {!selectedCategory &&
+                <>
+                    <div className={"page-title"}>
+                        Thèmes
+                    </div>
+                    <div className={"page-subtitle"}>Tes objectifs en cours</div>
+                    <div className="categories-wrapper">
+                        {categories.map((category, index) => (
+                            <div onClick={() => setSelectedCategory(category)} className={"category-title"} key={index}>
+                                <span>{category.title}</span><br/>
+                                <span>{category.progression}</span>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            }
+            {selectedCategory &&
+            <>
+                <div onClick={() => setSelectedCategory(null)}>picto retour</div>
+                <div className={""}>
+                    <div>image du thème</div>
+                    <div>{selectedCategory.title}</div>
+                </div>
+                <hr/>
+                <div>5 bonnes pratiques</div>
+                {selectedCategory.goals.map((goal, index) => (
+                    <div className={""} key={index}>
+                        <span>photo du goal</span>
+                        <span>{goal.title}</span>
+                        <div>{goal.description}</div>
+                        <div>
+                            <span>2 amis l'ont fait</span>
+                            <div>Voir plus</div>
+                        </div>
                     </div>
                 ))}
-            </div>
+            </>
+            }
         </div>
     )
 };
