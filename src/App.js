0f3a31dd592ca2f8802ab96c046a11fcdc78f3c2
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-26 18:09:46 +0100

    Add category level selection

--- src/App.js
+++ src/App.js
@@ -4,27 +4,51 @@ import './App.css';
 function App() {
   const [categories, setCategories] = useState([
     {
-      name: 'emails'
+      name: 'emails',
+      level: 5
     },
     {
-      name: 'séries'
+      name: 'séries',
+      level : 7
     },
     {
-      name: 'moteur de recherche'
+      name: 'moteur de recherche',
+      level: 8
     }
   ]);
   const [selectedCategory, setSelectedCategory] = useState();
-  const [categoryLevel, setCategoryLevel] = useState();
+  const [categoryLevel, setCategoryLevel] = useState(1);
+
+  const handleSelectedCategory = (category) => {
+    setCategoryLevel(1);
+    setSelectedCategory(category);
+  };
+
+  const handleLevelChange = (flow) => {
+      if (flow === 'down' && categoryLevel > 1) {
+          setCategoryLevel(categoryLevel - 1);
+      }
+      if (flow === 'up' && categoryLevel < selectedCategory.level) {
+          setCategoryLevel(categoryLevel + 1)
+      }
+  };
 
   return (
       <div>
         <span>les catégories</span>
         <ul>
           {categories.map((category, index) => (
-              <li key={index} onClick={() => setSelectedCategory(category)}>{category.name}</li>
+              <li key={index} onClick={() => handleSelectedCategory(category)}>{category.name}</li>
           ))}
         </ul>
-        <span>catégorie selectionnée : {selectedCategory ? selectedCategory.name : ""}</span>
+          {selectedCategory &&
+          <>
+              <span>catégorie selectionnée : {selectedCategory ? selectedCategory.name : ""}</span><br/>
+              <span>niveau actuel : {categoryLevel} / {selectedCategory.level}</span><br/>
+              <span onClick={() => handleLevelChange('down')}>down</span><br/>
+              <span onClick={() => handleLevelChange('up')}>up</span>
+          </>
+          }
       </div>
   );
 }
