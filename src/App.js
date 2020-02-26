d27d127f3992f40a8b0462d4c2fb511ae67b57be
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-26 23:00:40 +0100

    Add default fixture data and basic app states

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
+import categories from "data/categories.json";
 
 function App() {
-  const [categories, setCategories] = useState([
-    {
-      name: 'emails',
-      level: 5
-    },
-    {
-      name: 'séries',
-      level : 7
-    },
-    {
-      name: 'moteur de recherche',
-      level: 8
-    }
-  ]);
-  const [selectedCategory, setSelectedCategory] = useState();
-  const [categoryLevel, setCategoryLevel] = useState(1);
-
-  const handleSelectedCategory = (category) => {
-    setCategoryLevel(1);
-    setSelectedCategory(category);
-  };
-
-  const handleLevelChange = (flow) => {
-      if (flow === 'down' && categoryLevel > 1) {
-          setCategoryLevel(categoryLevel - 1);
-      }
-      if (flow === 'up' && categoryLevel < selectedCategory.level) {
-          setCategoryLevel(categoryLevel + 1)
-      }
-  };
+    const [currentPage, setCurrentPage] = useState(0);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedGoal, setSelectedGoal] = useState(null);
+    const [mascotView, setMascotView] = useState(false);
 
   return (
       <div>
-        <span>les catégories</span>
-        <ul>
-          {categories.map((category, index) => (
-              <li key={index} onClick={() => handleSelectedCategory(category)}>{category.name}</li>
-          ))}
-        </ul>
-          {selectedCategory &&
-          <>
-              <span>catégorie selectionnée : {selectedCategory ? selectedCategory.name : ""}</span><br/>
-              <span>niveau actuel : {categoryLevel} / {selectedCategory.level}</span><br/>
-              <span onClick={() => handleLevelChange('down')}>down</span><br/>
-              <span onClick={() => handleLevelChange('up')}>up</span>
-          </>
-          }
       </div>
   );
 }
