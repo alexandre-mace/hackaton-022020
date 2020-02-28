9426efb58518398487246a4328e300e461d77a75
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-28 07:05:34 +0100

    Updates

---
+++ precache-manifest.0447868e1e3bac279903468c1e0815ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae9421250a4ba408487f928421ba983",
+    "url": "/hackaton-022020/index.html"
+  },
+  {
+    "revision": "e3c7a22146b2cfa9ff6a",
+    "url": "/hackaton-022020/static/css/main.d72cc4f9.chunk.css"
+  },
+  {
+    "revision": "309bea347dd8e4f67cb6",
+    "url": "/hackaton-022020/static/js/2.4ba2b563.chunk.js"
+  },
+  {
+    "revision": "28691fa6a09912f222262c1dd6c9432c",
+    "url": "/hackaton-022020/static/js/2.4ba2b563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c7a22146b2cfa9ff6a",
+    "url": "/hackaton-022020/static/js/main.ef95360b.chunk.js"
+  },
+  {
+    "revision": "61da8938c9c297ad95d3",
+    "url": "/hackaton-022020/static/js/runtime-main.139218dd.js"
+  }
+]);
