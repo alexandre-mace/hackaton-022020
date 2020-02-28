535821c7fd1bbf4d27d6f2dedcda7828b4fa292c
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-28 09:20:47 +0100

    Fix img issue

--- src/page/challenge/FriendLink.js
+++ src/page/challenge/FriendLink.js
@@ -15,7 +15,7 @@ const FriendLink = ({setChallengeStep}) => {
 
       <div className="link__container">
         <p>https://cutt.ly/Cr4HqEV</p>
-        <button className="link__button" onClick={() => onCopyLink('https://cutt.ly/Cr4HqEV')}><img width="19" height="22" src='./../assets/images/ic_copy.png' /></button>
+        <button className="link__button" onClick={() => onCopyLink('https://cutt.ly/Cr4HqEV')}><img width="19" height="22" src={"assets/images/ic_copy.png"} /></button>
       </div>
       <div className="share"><button className="share__button">Partager le lien</button></div>
       <button className='challenge__button mt-auto' onClick={() => setChallengeStep(2)}>Commencez une partie</button>
